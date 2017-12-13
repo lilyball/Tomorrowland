53a84bec4015d52b1e1d70079f73a8dfb33f9ada
Kevin Ballard <kevin@sb.org>
2017-12-12 20:28:38 -0800

    Initial Commit

---
+++ Promissory/Promissory.h
@@ -0,0 +1,19 @@
+//
+//  Promissory.h
+//  Promissory
+//
+//  Created by Kevin Ballard on 12/12/17.
+//  Copyright © 2017 Kevin Ballard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Promissory.
+FOUNDATION_EXPORT double PromissoryVersionNumber;
+
+//! Project version string for Promissory.
+FOUNDATION_EXPORT const unsigned char PromissoryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Promissory/PublicHeader.h>
+
+
