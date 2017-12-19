d98f2392da4bdc118245e9cc81657ad9300f0288
Kevin Ballard <kevin@sb.org>
2017-12-18 23:42:49 -0800

    Write some initial tests

--- PromissoryTests/PromissoryTests.swift
+++ PromissoryTests/PromissoryTests.swift
@@ -7,32 +7,109 @@
 //
 
 import XCTest
-@testable import Promissory
+import Promissory
 
-class PromissoryTests: XCTestCase {
+final class PromissoryTests: XCTestCase {
     
     // Test to make sure `then(on: context, { /* return a fulfilled promise */ })` returns an already-fulfilled promise.
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testBasicFulfill() {
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.fulfill(42)
+        })
+        let expectation = XCTestExpectation(onSuccess: promise, expectedValue: 42)
+        wait(for: [expectation], timeout: 1)
+        XCTAssertEqual(promise.result, .value(42))
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    func testBasicReject() {
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.reject("error")
+        })
+        let expectation = XCTestExpectation(onError: promise, expectedError: "error")
+        wait(for: [expectation], timeout: 1)
+        XCTAssertEqual(promise.result, .error("error"))
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testBasicCancel() {
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.cancel()
+        })
+        let expectation = XCTestExpectation(onCancel: promise)
+        wait(for: [expectation], timeout: 1)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testAlreadyFulfilled() {
+        let promise = Promise<Int,String>(fulfilled: 42)
+        XCTAssertEqual(promise.result, .value(42))
+        var invoked = false
+        _ = promise.then(on: .immediate) { (x) in
+            invoked = true
         }
+        XCTAssertTrue(invoked)
     }
     
+    func testAlreadyRejected() {
+        let promise = Promise<Int,String>(rejected: "foo")
+        XCTAssertEqual(promise.result, .error("foo"))
+        var invoked = false
+        _ = promise.catch(on: .immediate) { (x) in
+            invoked = true
+        }
+        XCTAssertTrue(invoked)
+    }
+    
+    func testThenResult() {
+        let promise = Promise<Int,String>(fulfilled: 42).then(on: .utility) { (x) in
+            return x + 1
+        }
+        let expectation = XCTestExpectation(onSuccess: promise, expectedValue: 43)
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testThenReturningFulfilledPromise() {
+        let innerExpectation = XCTestExpectation(description: "Inner promise success")
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.fulfill(42)
+        }).then(on: .utility) { (x) -> Promise<String,String> in
+            let newPromise = Promise<String,String>(on: .utility) { resolver in
+                resolver.fulfill("\(x+1)")
+            }
+            innerExpectation.fulfill(onSuccess: newPromise, expectedValue: "43")
+            return newPromise
+        }
+        let outerExpectation = XCTestExpectation(onSuccess: promise, expectedValue: "43")
+        wait(for: [innerExpectation, outerExpectation], timeout: 1)
+    }
+    
+    func testThenReturningRejectedPromise() {
+        let innerExpectation = XCTestExpectation(description: "Inner promise error")
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.fulfill(42)
+        }).then(on: .utility) { (x) -> Promise<String,String> in
+            let newPromise = Promise<String,String>(on: .utility) { resolver in
+                resolver.reject("foo")
+            }
+            innerExpectation.fulfill(onError: newPromise, expectedError: "foo")
+            return newPromise
+        }
+        let outerExpectation = XCTestExpectation(onError: promise, expectedError: "foo")
+        wait(for: [innerExpectation, outerExpectation], timeout: 1)
+    }
+    
+    func testThenReturningCancelledPromise() {
+        let innerExpectation = XCTestExpectation(description: "Inner promise cancelled")
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.fulfill(42)
+        }).then(on: .utility) { (x) -> Promise<String,String> in
+            let newPromise = Promise<String,String>(on: .utility) { resolver in
+                resolver.cancel()
+            }
+            innerExpectation.fulfill(onCancel: newPromise)
+            return newPromise
+        }
+        let outerExpectation = XCTestExpectation(onCancel: promise)
+        wait(for: [innerExpectation, outerExpectation], timeout: 1)
+    }
+    }
 }
--- PromissoryTests/XCTestExpectation+Promise.swift
+++ PromissoryTests/XCTestExpectation+Promise.swift
@@ -10,12 +10,98 @@ import XCTest
 import Promissory
 
 extension XCTestExpectation {
+    convenience init<T,E>(description: String? = nil, onSuccess promise: Promise<T,E>, handler: @escaping (T) -> Void) {
+        self.init(description: description ?? "Expectation for \(type(of: promise)) success")
+        assertForOverFulfill = true
+        fulfill(onSuccess: promise, handler: handler)
+    }
     
+    convenience init<T,E>(description: String? = nil, onSuccess promise: Promise<T,E>, expectedValue: T) where T: Equatable {
+        self.init(description: description, onSuccess: promise) { (value) in
+            XCTAssertEqual(expectedValue, value, "Promise value")
+        }
+    }
+    
+    convenience init<T,E>(description: String? = nil, onError promise: Promise<T,E>, handler: @escaping (E) -> Void) {
+        self.init(description: description ?? "Expectation for \(type(of: promise)) error")
+        assertForOverFulfill = true
+        fulfill(onError: promise, handler: handler)
+    }
+    
+    convenience init<T,E>(description: String? = nil, onError promise: Promise<T,E>, expectedError: E) where E: Equatable {
+        self.init(description: description, onError: promise) { (error) in
+            XCTAssertEqual(expectedError, error, "Promise error")
+        }
+    }
+    
+    convenience init<T,E>(description: String? = nil, onCancel promise: Promise<T,E>) {
+        self.init(description: description ?? "Expectation for \(type(of: promise)) cancel")
+        assertForOverFulfill = true
+        fulfill(onCancel: promise)
+    }
+    
+    func fulfill<T,E>(onSuccess promise: Promise<T,E>, handler: @escaping (T) -> Void) {
+        promise.always(on: .default) { (result) in
+            switch result {
+            case .value(let value):
+                handler(value)
+                self.fulfill()
+            case .error(let error):
+                XCTFail("Expected Promise success, got error \(error)")
+                self.fulfill()
+            case .cancelled:
+                XCTFail("Expected Promise success, got cancellation")
+                self.fulfill()
+            }
+        }
+    }
+    
+    func fulfill<T,E>(onSuccess promise: Promise<T,E>, expectedValue: T) where T: Equatable {
+        fulfill(onSuccess: promise) { (value) in
+            XCTAssertEqual(expectedValue, value, "Promise value")
+        }
+    }
+    
+    func fulfill<T,E>(onError promise: Promise<T,E>, handler: @escaping (E) -> Void) {
+        promise.always(on: .default) { (result) in
+            switch result {
+            case .value(let value):
+                XCTFail("Expected Promise failure, got value \(value)")
+                self.fulfill()
+            case .error(let error):
+                handler(error)
+                self.fulfill()
+            case .cancelled:
+                XCTFail("Expected Promise success, got cancellation")
+                self.fulfill()
+            }
+        }
+    }
+    
+    func fulfill<T,E>(onError promise: Promise<T,E>, expectedError: E) where E: Equatable {
+        fulfill(onError: promise) { (error) in
+            XCTAssertEqual(expectedError, error, "Promise error")
+        }
+    }
+    
+    func fulfill<T,E>(onCancel promise: Promise<T,E>) {
+        promise.always(on: .default) { (result) in
+            switch result {
+            case .value(let value):
+                XCTFail("Expected Promise cancellation, got value: \(value)")
+                self.fulfill()
+            case .error(let error):
+                XCTFail("Expected Promise cancellation, got error \(error)")
+                self.fulfill()
+            case .cancelled:
+                self.fulfill()
+            }
+        }
+    }
 }
 
-class PromiseExpectation: XCTestExpectation {
-    init<T,E>(description: String? = nil, onSuccess promise: Promise<T,E>) {
-        super.init(description: description ?? "Expectation for \(type(of: promise)) success")
-        
-    }
+func XCTAssertEqual<T,E>(_ lhs: PromiseResult<T,E>?, _ rhs: PromiseResult<T,E>?, message: String? = nil, file: StaticString = #file, line: UInt = #line)
+    where T: Equatable, E: Equatable
+{
+    XCTAssert(lhs == rhs, "\(lhs.map(String.init(describing:)) ?? "nil") and \(rhs.map(String.init(describing:)) ?? "nil") are not equal\(message.map({ "; \($0)" }) ?? "")", file: file, line: line)
 }
