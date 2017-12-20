7712df11ef620c9cace47e21c67ae5ddc0683f08
Kevin Ballard <kevin@sb.org>
2017-12-19 21:01:41 -0800

    Add more tests

---
+++ Promissory/PromiseUtilities.swift
@@ -0,0 +1,9 @@
+//
+//  PromiseUtilities.swift
+//  Promissory
+//
+//  Created by Ballard, Kevin on 12/20/17.
+//  Copyright © 2017 Kevin Ballard. All rights reserved.
+//
+
+import Foundation
--- PromissoryTests/PromissoryTests.swift
+++ PromissoryTests/PromissoryTests.swift
@@ -135,6 +135,130 @@ final class PromissoryTests: XCTestCase {
         XCTAssertTrue(invoked)
     }
     
+    func testCatch() {
+        let expectation = XCTestExpectation(description: "catch")
+        _ = Promise<Int,String>(rejected: "foo").catch { (x) in
+            XCTAssertEqual(x, "foo")
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testRecover() {
+        let promise = Promise<Int,String>(rejected: "foo").recover({ (x) in
+            return 42
+        })
+        let expectation = XCTestExpectation(onSuccess: promise, expectedValue: 42)
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testRecoverReturningPromise() {
+        let promise = Promise<Int,String>(rejected: "foo").recover({ (x) in
+            return Promise(rejected: true)
+        })
+        let expectation = XCTestExpectation(onError: promise, expectedError: true)
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    // MARK: - Specializations for Error
+    
+    struct TestError: Error {}
+    
+    func testPromiseThrowingError() {
+        let promise = Promise<Int,Error>(on: .utility, { (resolver) in
+            throw TestError()
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testPromiseThenThrowingError() {
+        let promise = Promise<Int,Error>(fulfilled: 42).then(on: .utility, { (x) -> Int in
+            throw TestError()
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testPromiesThenThrowableReturningPromise() {
+        func handler(_ x: Int) throws -> Promise<String,Error> {
+            return Promise(rejected: TestError())
+        }
+        let promise = Promise<Int,Error>(fulfilled: 42).then(on: .utility, { (x) in
+            // Don't replace this block literal with handler directly, this tests to make sure we
+            // can infer the call without specifying a return type
+            return try handler(x)
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testPromiesThenThrowableReturningErrorCompatiblePromise() {
+        func handler(_ x: Int) throws -> Promise<String,TestError> {
+            return Promise(rejected: TestError())
+        }
+        let promise = Promise<Int,Error>(fulfilled: 42).then(on: .utility, { (x) in
+            // Don't replace this block literal with handler directly, this tests to make sure we
+            // can infer the call without specifying a return type
+            return try handler(x)
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testPromiseRecoverThrowingError() {
+        struct DummyError: Error {}
+        let promise = Promise<Int,Error>(rejected: DummyError()).recover(on: .utility, { (error) -> Int in
+            throw TestError()
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testPromiseRecoverThrowableReturningPromise() {
+        struct DummyError: Error {}
+        func handler(_ error: Error) throws -> Promise<Int,Error> {
+            return Promise(rejected: TestError())
+        }
+        let promise = Promise<Int,Error>(rejected: DummyError()).recover(on: .utility, { (error) in
+            // Don't replace this block literal with handler directly, this tests to make sure we
+            // can infer the call without specifying a return type
+            return try handler(error)
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    func testPromiseRecoverThrowableReturningErrorCompatiblePromise() {
+        struct DummyError: Error {}
+        func handler(_ error: Error) throws -> Promise<Int,TestError> {
+            return Promise(rejected: TestError())
+        }
+        let promise = Promise<Int,Error>(rejected: DummyError()).recover(on: .utility, { (error) in
+            // Don't replace this block literal with handler directly, this tests to make sure we
+            // can infer the call without specifying a return type
+            return try handler(error)
+        })
+        let expectation = XCTestExpectation(onError: promise) { (error) in
+            XCTAssert(error is TestError)
+        }
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    // MARK: -
+    
     func testPromiseContexts() {
         var expectations: [XCTestExpectation] = []
         let mainExpectation = XCTestExpectation(description: "main context")
@@ -266,6 +390,54 @@ final class PromissoryTests: XCTestCase {
         token.invalidate()
         sema.signal()
         wait(for: [expectation], timeout: 1)
+    }
+    
+    func testResolvingFulfilledPromise() {
+        // Resolving a promise that has already been fulfliled does nothing
+        let expectation = XCTestExpectation(description: "promise")
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.fulfill(42)
+            resolver.fulfill(43)
+            resolver.reject("error")
+            resolver.cancel()
+            expectation.fulfill()
+        })
+        let expectation2 = XCTestExpectation(onSuccess: promise, expectedValue: 42)
+        wait(for: [expectation, expectation2], timeout: 1)
+        // now that the promise is resolved, check again to make sure the value is the same
+        XCTAssertEqual(promise.result, .value(42))
+    }
+    
+    func testResolvingRejectedPromise() {
+        // Resolving a promise that has already been rejected does nothing
+        let expectation = XCTestExpectation(description: "promise")
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.reject("error")
+            resolver.reject("foobar")
+            resolver.fulfill(43)
+            resolver.cancel()
+            expectation.fulfill()
+        })
+        let expectation2 = XCTestExpectation(onError: promise, expectedError: "error")
+        wait(for: [expectation, expectation2], timeout: 1)
+        // now that the promise is resolved, check again to make sure the value is the same
+        XCTAssertEqual(promise.result, .error("error"))
+    }
+    
+    func testResolvingCancelledPromise() {
+        // Resolving a promise that has already been cancelled does nothing
+        let expectation = XCTestExpectation(description: "promise")
+        let promise = Promise<Int,String>(on: .utility, { (resolver) in
+            resolver.cancel()
+            resolver.cancel()
+            resolver.reject("foobar")
+            resolver.fulfill(43)
+            expectation.fulfill()
+        })
+        let expectation2 = XCTestExpectation(onCancel: promise)
+        wait(for: [expectation, expectation2], timeout: 1)
+        // now that the promise is resolved, check again to make sure the value is the same
+        XCTAssertEqual(promise.result, .cancelled)
     }
 }
 
