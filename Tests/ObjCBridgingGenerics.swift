83b952522dd715a64c207d603a2f74f238030e00
Kevin Ballard <kevin@sb.org>
2018-03-25 12:08:01 -0700

    Add file to test target to prove lack of ambiguity in bridging
    This demonstrates a simple wrapper around the bridging machinery and
    proves that such a wrapper can be written without hitting ambiguity
    errors.

---
+++ Tests/ObjCBridgingGenerics.swift
@@ -0,0 +1,64 @@
+//
+//  ObjCBridgingGenerics.swift
+//  TomorrowlandTests
+//
+//  Created by Kevin Ballard on 3/25/18.
+//  Copyright © 2018 Kevin Ballard. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+//  http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+//  <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+//  option. This file may not be copied, modified, or distributed
+//  except according to those terms.
+//
+
+// This is a simple generic wrapper around the ObjC promise bridging machinery.
+// It's meant to prove that there's no ambiguity when using generics.
+
+import Tomorrowland
+import Foundation.NSError
+
+private protocol _BridgeToObjC {
+    associatedtype _ObjCType: AnyObject
+    func _bridgeToObjC() -> _ObjCType
+}
+
+private protocol _BridgeToSwift {
+    associatedtype _SwiftType
+    func _bridgeToSwift() -> _SwiftType
+}
+
+private extension Promise {
+    init<V, E>(_ promise: ObjCPromise<V,E>)
+        where V: _BridgeToSwift, V._SwiftType == Value,
+        E: _BridgeToSwift, E._SwiftType == Error
+    {
+        self.init(bridging: promise, mapValue: { $0._bridgeToSwift() }, mapError: { $0._bridgeToSwift() })
+    }
+}
+
+private extension Promise where Error == Swift.Error {
+    init<V>(_ promise: ObjCPromise<V,NSError>)
+        where V: _BridgeToSwift, V._SwiftType == Value
+    {
+        self.init(bridging: promise, mapValue: { $0._bridgeToSwift() })
+    }
+}
+
+private extension Promise where Value: _BridgeToObjC {
+    func objc<E: AnyObject>(mapError: @escaping (Error) -> E) -> ObjCPromise<Value._ObjCType, E> {
+        return objc(mapValue: { $0._bridgeToObjC() }, mapError: mapError)
+    }
+}
+
+private extension Promise where Error: _BridgeToObjC {
+    func objc<V: AnyObject>(mapValue: @escaping (Value) -> V) -> ObjCPromise<V, Error._ObjCType> {
+        return objc(mapValue: mapValue, mapError: { $0._bridgeToObjC() })
+    }
+}
+
+private extension Promise where Value: _BridgeToObjC, Error: _BridgeToObjC {
+    func objc() -> ObjCPromise<Value._ObjCType, Error._ObjCType> {
+        return objc(mapValue: { $0._bridgeToObjC() }, mapError: { $0._bridgeToObjC() })
+    }
+}
