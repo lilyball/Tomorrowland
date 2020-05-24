4fcfdc8c1577abf425ece4465f394be7ab8b5ec9
Lily Ballard <lily@sb.org>
2020-05-23 18:45:19 -0700

    Fix Promise.timeout's default context for the Error overload

--- README.md
+++ README.md
@@ -441,6 +441,7 @@ Unless you explicitly state otherwise, any contribution intentionally submitted 
   `.immediate` and running synchronously, it inherits the surrounding scope's `PromiseContext.isExecutingNow` flag. This is intended to allow
   `Promise(on: .immediate, { … })` to query the surrounding scope's flag ([#53][]).
 - Add convenience methods to Obj-C for doing then+catch together, as this is a common pattern and chaining Obj-C methods is a little awkward ([#45][]).
+- Change `Promise.timeout`'s default context to `.nowOr(.auto)` for the `Error` overload as well.
 
 [#45]: https://github.com/lilyball/Tomorrowland/issues/45 "Add convenience methods to Obj-C for then+catch"
 [#53]: https://github.com/lilyball/Tomorrowland/issues/53 "Can we add something like PromiseContext.isExecutingNow?"
--- Sources/Utilities.swift
+++ Sources/Utilities.swift
@@ -248,7 +248,11 @@ extension Promise where Error == Swift.Error {
     /// will be rejected with the same error.
     ///
     /// - Parameter context: The context to invoke the callback on. If not provided, defaults to
-    ///   `.auto`, which evaluates to `.main` when invoked on the main thread, otherwise `.default`.
+    ///   `.nowOr(.auto)`, which evaluates to `.nowOr(.main)` when invoked on the main thread,
+    ///   otherwise `.nowOr(.default)`. The usage of `.nowOr` here means that if the receiver has
+    ///   already been resolved when this method is called, the returned promise will likewise
+    ///   already be resolved. If the receiver has not already resolved then this behaves the same
+    ///   as passing `.auto`.
     ///
     ///   If the promise times out, the returned promise will be rejected using the same context. In
     ///   this event, `.immediate` is treated the same as `.auto`. If provided as `.operationQueue`
@@ -258,7 +262,7 @@ extension Promise where Error == Swift.Error {
     ///   zero, the returned `Promise` will be timed out at once unless the receiver is already
     ///   resolved.
     /// - Returns: A new `Promise`.
-    public func timeout(on context: PromiseContext = .auto, delay: TimeInterval) -> Promise<Value,Swift.Error> {
+    public func timeout(on context: PromiseContext = .nowOr(.auto), delay: TimeInterval) -> Promise<Value,Swift.Error> {
         let (promise, resolver) = Promise<Value,Error>.makeWithResolver()
         let propagateCancelBlock = TWLOneshotBlock(block: { [weak _box] in
             _box?.propagateCancel()
